e478e3d08669972d2831ef8f7692906f5870bfe3
Jack Gilbert <jeg6835@rit.edu>
2023-02-26 14:49:30 -0500

    fixing styling to be more scalable

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/GoogleMap.module.css';
 const GoogleMap: React.FC = () => {
   return (
     <div>
-        <img src={"https://via.placeholder.com/300x200.png?text=MAP"} className={styles.image} />
+        <img src={"https://via.placeholder.com/1520x500.png?text=MAP"} className={styles.image} />
     </div>
   );
 };
