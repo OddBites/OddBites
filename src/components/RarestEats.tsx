996a6854f7a07109a8098b3f6bda8ba3f30ec50d
Jack Gilbert <jeg6835@rit.edu>
2023-03-03 11:29:05 -0500

    fixing to use foodpanel component rather than a restaurant component

--- src/components/RarestEats.tsx
+++ src/components/RarestEats.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RestaurantPanel from './RestaurantPanel';
+import FoodPanel from './FoodPanel';
 import styles from '../styles/RarestEats.module.css';
 
 const food = [
@@ -7,31 +7,13 @@ const food = [
     name: "Food 1",
     description: "Description 1",
     image: "https://via.placeholder.com/300x200.png?text=Food+1",
+    nutrition: "30",
   },
   {
     name: "Food 2",
     description: "Description 2",
     image: "https://via.placeholder.com/300x200.png?text=Food+2",
-  },
-  {
-    name: "Food 3",
-    description: "Description 3",
-    image: "https://via.placeholder.com/300x200.png?text=Food+3",
-  },
-  {
-    name: "Food 4",
-    description: "Description 4",
-    image: "https://via.placeholder.com/300x200.png?text=Food+4",
-  },
-  {
-    name: "Food 5",
-    description: "Description 5",
-    image: "https://via.placeholder.com/300x200.png?text=Food+4",
-  },
-  {
-    name: "Food 6",
-    description: "Description 6",
-    image: "https://via.placeholder.com/300x200.png?text=Food+5",
+    nutrition: "60",
   }
 ];
 
@@ -40,11 +22,12 @@ const RarestEats: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.restaurantGrid}>
         {food.map((food) => (
-          <RestaurantPanel
+          <FoodPanel
             key={food.name}
             name={food.name}
             description={food.description}
             image={food.image}
+            nutrition={food.nutrition}
           />
         ))}
       </div>
