e0c03f426365dbf2a637c35413cddb7c5dcb9d95
Jack Gilbert <jeg6835@rit.edu>
2023-03-08 01:36:21 -0500

    fixed some styling with the column bar

--- src/components/BottomNav/BottomSide.tsx
+++ src/components/BottomNav/BottomSide.tsx
@@ -5,22 +5,32 @@ import Favorites from './Favorites';
 import styles from '../../styles/bottomSide.module.css';
 
 const BottomSide: React.FC = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.popularRestaurants}>
-                <div className={styles.title}> Popular Restaurants</div>
-                <PopularRestaurants />
-            </div>
-            <div className={styles.rarestEats}>
-                <div className={styles.title}> Rarest Eats</div>
-                <RarestEats />
-            </div>
-            <div className={styles.favoritesScroll}>
-                <div className={styles.title}>Favorites</div>
-                <Favorites />
-            </div>
+  const popularRestaurants = <PopularRestaurants />;
+  const rarestEats = <RarestEats />;
+  const favorites = <Favorites />;
+
+  return (
+    <div className={styles.container}>
+      {popularRestaurants && (
+        <div className={styles.popularRestaurants}>
+          <div className={styles.title}> Popular Restaurants</div>
+          {popularRestaurants}
         </div>
-    );
+      )}
+      {rarestEats && (
+        <div className={styles.rarestEats}>
+          <div className={styles.title}> Rarest Eats</div>
+          {rarestEats}
+        </div>
+      )}
+      {favorites && (
+        <div className={styles.favoritesScroll}>
+          <div className={styles.title}>Favorites</div>
+          {favorites}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BottomSide;
