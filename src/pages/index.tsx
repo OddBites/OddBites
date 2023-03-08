31473ca162f716681acc8beadca7ef4dfcecdc8c
Jack Gilbert <jeg6835@rit.edu>
2023-03-08 02:17:44 -0500

    made it so the page is auto routed to the restaurants

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import App from './_app'
-import Link from 'next/link';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const HomePage: React.FC = () => {
+  const router = useRouter();
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark");
-}
+  useEffect(() => {
+    router.replace('/restaurants');
+  }, [router]);
 
+  return null;
+};
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>OddBites</title>
-        <meta name="description" content="OddBites" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-      <div className={styles.description}>
-      <button onClick={myFunction}>Switch Modes</button>
-          <p>
-            Hello Everyone!
-          </p>
-          </div>
-    </main>
-    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfJgOybNOb9-nAD3Rxb2avRCktO6nymPU&callback=initMap"></script>
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
 
-    </>
-  )
-}
+export default HomePage;
